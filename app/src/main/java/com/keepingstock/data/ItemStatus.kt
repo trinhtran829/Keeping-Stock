8ed411ae0e52e215654cadf2df8cf9e7b224ce92
trintran829 <trintran@oregonstate.edu>
2026-01-29 21:26:20 -0800

    Adding citations

--- app/src/main/java/com/keepingstock/data/ItemStatus.kt
+++ app/src/main/java/com/keepingstock/data/ItemStatus.kt
@@ -1,5 +1,12 @@
 package com.keepingstock.data
 
+/*
+* This code was generated with the help of Android Basics with Compose course.
+* This link
+* https://developer.android.com/codelabs/basic-android-kotlin-compose-generics?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-3-pathway-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-generics#2
+* documents the sample code that led to my code.
+*/
+
 enum class ItemStatus {
     STORED,
     TAKEN_OUT
--- app/src/main/java/com/keepingstock/data/ItemWithTags.kt
+++ app/src/main/java/com/keepingstock/data/ItemWithTags.kt
@@ -4,6 +4,13 @@ import androidx.room.Embedded
 import androidx.room.Junction
 import androidx.room.Relation
 
+/*
+* This code was generated with the help of Android Basics with Compose course.
+* This link
+* https://developer.android.com/training/data-storage/room/relationships/many-to-many
+* documents the sample code that led to my code.
+*/
+
 data class ItemWithTags (
     @Embedded val item: ItemEntity,
     @Relation(
