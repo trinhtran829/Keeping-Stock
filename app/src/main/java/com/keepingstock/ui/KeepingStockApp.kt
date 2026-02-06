b014c3190bb04b6979c0031dfed988c301dff772
damcsk <davidmcskaggs@gmail.com>
2026-02-05 21:08:38 -0600

    Add scaffold to KeepingStockApp

--- app/src/main/java/com/keepingstock/ui/KeepingStockApp.kt
+++ app/src/main/java/com/keepingstock/ui/KeepingStockApp.kt
@@ -1,11 +1,27 @@
 package com.keepingstock.ui
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.keepingstock.ui.navigation.AppNavGraph
 
 @Preview(showSystemUi = false, showBackground = true)
 @Composable
 fun KeepingStockApp() {
-    AppNavGraph()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
+        topBar = {} // TODO
+    ) { innerPadding ->
+        AppNavGraph(
+            modifier = Modifier,
+            contentPadding = innerPadding
+        )
+    }
+    
 }
