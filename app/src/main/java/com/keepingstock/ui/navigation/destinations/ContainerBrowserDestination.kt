9b4c355d2995adca40c672c1a260e48890ede377
damcsk <davidmcskaggs@gmail.com>
2026-02-06 01:23:40 -0600

    Refactor NavGraphBuilder composable functions outside AppNavGraph to keep separation of concerns
    # Conflicts:
    #	app/src/main/java/com/keepingstock/ui/navigation/AppNavGraph.kt

---
+++ app/src/main/java/com/keepingstock/ui/navigation/destinations/ContainerBrowserDestination.kt
@@ -0,0 +1,82 @@
+package com.keepingstock.ui.navigation.destinations
+
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.keepingstock.core.contracts.Routes
+import com.keepingstock.ui.navigation.NavDeps
+import com.keepingstock.ui.navigation.NavRoute
+import com.keepingstock.ui.scaffold.TopBarConfig
+import com.keepingstock.ui.screens.container.ContainerBrowserScreen
+
+internal fun NavGraphBuilder.addContainerBrowserDestination(
+    deps: NavDeps,
+    lastContainerIdState: MutableState<String?>
+) {
+    // Register the ContainerBrowser destination: when route == "container_browser" with or
+    // without the containerId, show ContainerBrowser of that container (or root)
+    composable(
+        route = NavRoute.ContainerBrowser.route,
+        arguments = listOf(
+            navArgument(Routes.Args.CONTAINER_ID) {
+                type = NavType.StringType
+                nullable = true
+                defaultValue = null
+            }
+        )
+    ) { backStackEntry ->
+        val containerId = backStackEntry.arguments?.getString(Routes.Args.CONTAINER_ID)
+
+        // TODO: Display container name in title instead of id
+        LaunchedEffect(containerId) {
+            deps.onTopBarChange(
+                TopBarConfig(
+                    title = (
+                        if (containerId == null)
+                            "Root Container Browser"
+                        else
+                            "Container $containerId Browser"
+                    ),
+                    showBack = containerId != null
+                )
+            )
+        }
+
+        // Track the last visited container for "Return to Containers" behavior.
+        lastContainerIdState.value = containerId
+
+        ContainerBrowserScreen(
+            containerId = containerId,
+            onOpenSubcontainer = { subId ->
+                deps.navController.navigate(NavRoute.ContainerBrowser.createRoute(subId))
+            },
+            onOpenItem = { itemId ->
+                deps.navController.navigate(NavRoute.ItemDetails.createRoute(itemId))
+            },
+            onOpenContainerInfo = { id ->
+                deps.navController.navigate(NavRoute.ContainerDetail.createRoute(id))
+            },
+            onGoToItemBrowser = {
+                deps.navController.navigate(NavRoute.ItemBrowser.route) {
+                    popUpTo(Routes.CONTAINER_BROWSER) { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            },
+            onAddContainer = { parentId ->
+                deps.navController.navigate(
+                    NavRoute.AddEditContainer.createRoute(parentContainerId = parentId)
+                )
+            },
+            onAddItem = { cid ->
+                deps.navController.navigate(NavRoute.AddEditItem.createRoute(containerId = cid))
+            },
+            onScanQr = {
+                deps.navController.navigate(NavRoute.QRScan.route)
+            }
+        )
+    }
+}
