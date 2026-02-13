9f2ba7f92926d8cc30c3fdf7ec009c32275fc336
damcsk <davidmcskaggs@gmail.com>
2026-02-13 01:51:25 -0600

    Add Basic circular progress indicator animation for loading state.

--- app/src/main/java/com/keepingstock/ui/screens/container/ContainerBrowserScreen.kt
+++ app/src/main/java/com/keepingstock/ui/screens/container/ContainerBrowserScreen.kt
@@ -3,10 +3,13 @@ package com.keepingstock.ui.screens.container
 import android.R.attr.top
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,6 +52,7 @@ fun ContainerBrowserScreen(
         )
     }
     /*
+    // TODO: OLD PLACEHOLDER UI - DELETE AFTER REFACTOR OF SCREEN
     Column(modifier = modifier.padding(16.dp)) {
         Text("Container Browser Screen (placeholder)")
         Text("containerId = ${containerId ?: "ROOT"}")
@@ -124,7 +128,9 @@ fun ContainerBrowserScreen(
  */
 @Composable
 private fun LoadingContent(modifier: Modifier) {
-
+    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
 }
 
 /**
