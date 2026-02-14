e7a5360addf3324f5c3fc2e18056f66699ff62f2
damcsk <davidmcskaggs@gmail.com>
2026-02-14 13:40:09 -0600

    Update ContainerDetailScreen to use each UiState

--- app/src/main/java/com/keepingstock/ui/screens/container/previews/ContainerDetailPreview.kt
+++ app/src/main/java/com/keepingstock/ui/screens/container/previews/ContainerDetailPreview.kt
@@ -1,6 +1,5 @@
 package com.keepingstock.ui.screens.container.previews
 
-import android.R.attr.name
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.keepingstock.core.contracts.Container
