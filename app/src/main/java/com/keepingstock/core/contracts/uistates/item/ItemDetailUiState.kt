7541b45e6bf5dc1b113a628fa537bbf0c5903ae7
damcsk <davidmcskaggs@gmail.com>
2026-02-14 23:37:38 -0600

    Add initial ItemDetailsUiState states

--- app/src/main/java/com/keepingstock/core/contracts/uistates/item/ItemDetailUiState.kt
+++ app/src/main/java/com/keepingstock/core/contracts/uistates/item/ItemDetailUiState.kt
@@ -1,2 +1,29 @@
 package com.keepingstock.core.contracts.uistates.item
 
+import com.keepingstock.core.contracts.ContainerId
+import com.keepingstock.core.contracts.ItemId
+import com.keepingstock.data.entities.ItemStatus
+
+/**
+ * UI state for the Item Details screen.
+ */
+sealed interface ItemDetailUiState {
+
+    /**
+     * Item details are being loaded
+     */
+    data object Loading : ItemDetailUiState
+
+    /**
+     * Item details successfully loaded and ready for display
+     */
+    data class Ready(
+        val itemId: ItemId,
+        val itemName: String,
+        val description: String?,
+        val imageUri: String?,
+        val parentContainerId: ContainerId?,
+        val parentContainerName: String?,
+        val status: ItemStatus
+    ) : ItemDetailUiState
+}
