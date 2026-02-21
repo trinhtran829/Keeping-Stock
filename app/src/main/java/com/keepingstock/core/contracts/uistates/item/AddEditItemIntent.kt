44671840cdf2bd9e725730bd0fb53fc5847f2e4b
damcsk <davidmcskaggs@gmail.com>
2026-02-21 16:30:34 -0600

    Add intent related states to AddEditItemIntent

--- app/src/main/java/com/keepingstock/core/contracts/uistates/item/AddEditItemIntent.kt
+++ app/src/main/java/com/keepingstock/core/contracts/uistates/item/AddEditItemIntent.kt
@@ -1,2 +1,28 @@
 package com.keepingstock.core.contracts.uistates.item
 
+import com.keepingstock.core.contracts.ContainerId
+import com.keepingstock.core.contracts.uistates.container.AddEditContainerIntent
+import com.keepingstock.data.entities.ItemStatus
+
+sealed interface AddEditItemIntent {
+    // Editable fields
+    data class NameChanged(val value: String) : AddEditItemIntent
+    data class DescriptionChanges(val value: String) : AddEditItemIntent
+
+    // Container/Status fields
+    data class ContainerChanged(val containerId: ContainerId?) : AddEditItemIntent
+    data class StatusChanged(val status: ItemStatus) : AddEditItemIntent
+
+    // Image related intent
+    data object PickImageClicked : AddEditItemIntent
+    data class ImagePicked(val uriString: String) : AddEditItemIntent
+    data object RemoveImageClicked : AddEditItemIntent
+
+    // Action intent
+    data object SaveClicked : AddEditItemIntent
+    data object BackClicked : AddEditItemIntent
+
+    // TODO: Intent related to navigating away
+    data object DiscardChangesConfirmed : AddEditItemIntent
+    data object DismissDiscardDialog : AddEditItemIntent
+}
