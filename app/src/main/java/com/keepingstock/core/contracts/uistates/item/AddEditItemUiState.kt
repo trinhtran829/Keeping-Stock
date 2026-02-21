16a8633b89fdd089ba162773d603fcb28900118b
damcsk <davidmcskaggs@gmail.com>
2026-02-20 21:58:50 -0600

    Add initial state + intent files.

---
+++ app/src/main/java/com/keepingstock/core/contracts/uistates/item/AddEditItemUiState.kt
@@ -0,0 +1,53 @@
+package com.keepingstock.core.contracts.uistates.item
+
+import com.keepingstock.core.contracts.ContainerId
+import com.keepingstock.core.contracts.ItemId
+import com.keepingstock.data.entities.ItemStatus
+
+sealed interface AddEditItemUiState {
+    /**
+     * Form is being loaded/initialized
+     */
+    data object Loading: AddEditItemUiState
+
+    /**
+     * Form is ready for display and/or editing
+     *
+     * Not using Item model because containerId needs to be able to be null
+     */
+    data class Ready(
+        val mode: Mode,
+        val itemId: ItemId?,              // null when creating
+        val containerId: ContainerId?,
+        val containerName: String?,           // need for displaying
+        val availableParents: List<ParentOption>,   // TODO: Improve for hierarchical display?
+        val name: String,
+        val description: String,
+        val status: ItemStatus,
+        val imageUri: String?,
+        val isSaving: Boolean = false,
+        val isDirty: Boolean = false,
+        val validation: Validation = Validation(),
+        val canChangeParent: Boolean = true
+    ) : AddEditItemUiState {
+        enum class Mode { CREATE, EDIT }
+
+        data class ParentOption(
+            val id: ContainerId?, // null = root
+            val name: String
+        )
+
+        // TODO: Anything else need to be validated? Tags?
+        data class Validation(
+            val nameError: String? = null
+        )
+    }
+
+    /**
+     * Error occurred while preparing the form.
+     */
+    data class Error(
+        val message: String,
+        val cause: Throwable? = null
+    ) : AddEditItemUiState
+}
