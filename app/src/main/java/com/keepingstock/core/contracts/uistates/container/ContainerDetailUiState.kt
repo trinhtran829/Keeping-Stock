03672932bad0d69daa05d88411398b3f17667e91
damcsk <davidmcskaggs@gmail.com>
2026-02-24 21:54:49 -0600

    Add ContainerDetailUiState notes for the Ready State for clarification of ViewModel usage.

--- app/src/main/java/com/keepingstock/core/contracts/uistates/container/ContainerDetailUiState.kt
+++ app/src/main/java/com/keepingstock/core/contracts/uistates/container/ContainerDetailUiState.kt
@@ -16,6 +16,36 @@ sealed interface ContainerDetailUiState {
 
     /**
      * Container details successfully loaded and ready for display.
+     *
+     * Count semantics:
+     * - [subcontainerCount] represents the number of direct child containers.
+     * - [itemCount] represents the number of items directly contained in this container.
+     * - These values are intended for display purposes only and should reflect repository-derived
+     *   counts at the time the state was emitted.
+     *
+     * Deletion rules:
+     * - [canDelete] determines whether the Delete action should be enabled in the UI.
+     * - When [canDelete] is false, [deleteBlockedReason] must contain a user-facing explanation
+     *   describing why deletion is not allowed.
+     * - When [canDelete] is true, [deleteBlockedReason] should be null.
+     *
+     * Invariants:
+     * - If [canDelete] is false, [deleteBlockedReason] must not be null.
+     * - If [canDelete] is true, [deleteBlockedReason] must be null.
+     *
+     * The ViewModel is responsible for:
+     * - Loading the container and its parent (if any).
+     * - Computing child container and item counts.
+     * - Enforcing deletion rules and populating the corresponding fields.
+     *
+     * @param container The container being displayed.
+     * @param parentContainerName The display name of the parent container, or null if this
+     *                            container is at the root level.
+     * @param createdDate The creation timestamp of the container.
+     * @param subcontainerCount Number of direct child containers.
+     * @param itemCount Number of direct items contained within this container.
+     * @param canDelete Whether the container can be deleted.
+     * @param deleteBlockedReason Explanation shown when deletion is not allowed.
      */
     data class Ready(
         val container: Container,
