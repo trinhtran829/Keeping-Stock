6a5f6d0fa40db276e3a8890c379f7f2ec9ebabd6
damcsk <davidmcskaggs@gmail.com>
2026-03-05 21:51:28 -0600

    Add skeleton intent contract for selection screen.

--- app/src/main/java/com/keepingstock/core/contracts/intents/utility/SelectContainerIntent.kt
+++ app/src/main/java/com/keepingstock/core/contracts/intents/utility/SelectContainerIntent.kt
@@ -1,2 +1,10 @@
 package com.keepingstock.core.contracts.intents.utility
 
+import com.keepingstock.core.contracts.Container
+
+sealed interface SelectContainerIntent {
+    data class EnterContainer(val container: Container?) : SelectContainerIntent
+
+    data object Cancel: SelectContainerIntent
+    data object Confirm: SelectContainerIntent
+}
