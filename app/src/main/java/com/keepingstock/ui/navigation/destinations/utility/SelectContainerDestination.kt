399ba9ae1d884a4ca0300cd8c419143a496c8b20
damcsk <davidmcskaggs@gmail.com>
2026-03-06 21:16:51 -0600

    Start implementing ViewModel

--- app/src/main/java/com/keepingstock/ui/navigation/destinations/utility/SelectContainerDestination.kt
+++ app/src/main/java/com/keepingstock/ui/navigation/destinations/utility/SelectContainerDestination.kt
@@ -1,5 +1,11 @@
 package com.keepingstock.ui.navigation.destinations.utility
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
@@ -9,6 +15,8 @@ import com.keepingstock.core.contracts.Routes.SubjectType
 import com.keepingstock.ui.navigation.NavDeps
 import com.keepingstock.ui.navigation.NavRoute
 import com.keepingstock.ui.navigation.containerIdOrNull
+import com.keepingstock.ui.viewmodel.utility.SelectContainerViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 internal fun NavGraphBuilder.addSelectContainerDestination(
     deps: NavDeps
@@ -29,5 +37,34 @@ internal fun NavGraphBuilder.addSelectContainerDestination(
         val subjectType = SubjectType.from(args.getString(Routes.Args.SUBJECT_TYPE)!!)
         val subjectId = args.getLong(Routes.Args.SUBJECT_ID)
         val currentContainerId = args.containerIdOrNull(Routes.Args.CONTAINER_ID)
+
+        val vm: SelectContainerViewModel = viewModel(
+            viewModelStoreOwner = backStackEntry,
+            factory = viewModelFactory {
+                initializer {
+                    SelectContainerViewModel(
+                        subjectType = subjectType,
+                        subjectId = subjectId,
+                        currentContainerId = currentContainerId,
+                        containerRepository = deps.containerRepo
+                    )
+                }
+            }
+        )
+
+        val uiState by vm.uiState.collectAsStateWithLifecycle()
+
+        LaunchedEffect(vm) {
+            vm.effects.collectLatest { effect ->
+                when (effect) {
+                    is SelectContainerViewModel.UiEffect.ReturnSelection -> TODO()
+
+                    is SelectContainerViewModel.UiEffect.ShowSnackbar -> TODO()
+
+                    SelectContainerViewModel.UiEffect.NavigateBack ->
+                        deps.navController.popBackStack()
+                }
+            }
+        }
     }
 }
